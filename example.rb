df88e7a8b9014bbdae6e787edfe04937185879f2
David Fisher <tibbon@gmail.com>
2014-11-18 16:48:10 -0500

    Updated the example

--- example.rb
+++ example.rb
@@ -3,16 +3,18 @@
 
 def format_num(num)
   num.gsub(/\D/,"")
-  # \D is any non-digit character
+  # gsub takes two arguments:
+  # the first is a regex pattern to match: \D is any non-digit character
+  # the second is what matching elements should be replaced with
 end
 
 nums = [
-"(646)-242-2234",
-"(516) 445-2190",
-"(718) 445 8999",
-"17320432111",
-"1-973-324-5532",
-"1(455)2301444",
+  '(646)-242-2234',
+  '(516) 445-2190',
+  '(718) 445 8999',
+  '17320432111',
+  '1-973-324-5532',
+  '1(455)2301444',
 ]
 
 nums.each_with_index { |e,i|
